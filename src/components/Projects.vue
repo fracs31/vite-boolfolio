7b2d74dcef2a35df27716743716312f573568d09
fracs31 <pist.fracs95@gmail.com>
2023-05-02 16:42:51 +0200

    ADD: pagination

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,19 +11,46 @@
         //Dati
         data() {
             return {
-                projects: [], //progetti
+                projects: [], //progetti,
+                currentPage: 1, //pagina corrente
+                links: [], //links
             }
         },
         //Metodi
         methods: {
             //Metodo che restituisce i progetti
-            fetchProjects() {
+            fetchProjects(page) {
                 //Effettuo la chiamata all'API
-                axios.get("http://127.0.0.1:8000/api/projects")
+                axios.get("http://127.0.0.1:8000/api/projects", {
+                    params: {
+                        page: page, //pagina
+                    }
+                })
                 .then((res) => {
                     const results = res.data.results; //salvo i risultati
-                    this.projects = results; //assegno i risultati ai progetti
-                    console.log(this.projects); //stampo in console i progetti
+                    this.projects = results.data; //assegno i risultati ai progetti
+                    this.currentPage = results.current_page; //pagina corrente
+                    this.links = results.links; //links
+                    console.log("Progetti: ", this.projects); //stampo in console i progetti
+                    console.log("Pagina corrente: ", this.currentPage); //stampo in console i progetti
+                    console.log("Links: ", this.links); //stampo in console i progetti
+                })
+                .catch((err) => {
+                    console.log(err); //stampo gli errori
+                });
+            },
+            //Metodo che restituisce i progetti
+            fetchProjectsByUrl(url) {
+                //Effettuo la chiamata all'API
+                axios.get(url)
+                .then((res) => {
+                    const results = res.data.results; //salvo i risultati
+                    this.projects = results.data; //assegno i risultati ai progetti
+                    this.currentPage = results.current_page; //pagina corrente
+                    this.links = results.links; //links
+                    console.log("Progetti: ", this.projects); //stampo in console i progetti
+                    console.log("Pagina corrente: ", this.currentPage); //stampo in console i progetti
+                    console.log("Links: ", this.links); //stampo in console i progetti
                 })
                 .catch((err) => {
                     console.log(err); //stampo gli errori
@@ -32,7 +59,7 @@
         },
         //Mounted
         mounted() {
-            this.fetchProjects(); //chiamata all'API dei progetti
+            this.fetchProjects(this.currentPage); //chiamata all'API dei progetti
         }
     }
 </script>
@@ -47,6 +74,12 @@
             <ProjectCard v-bind:key="project.id" v-bind:project="project"></ProjectCard>
         </li>
     </ul>
+    <!-- Links -->
+    <ul class="links">
+        <!-- Link -->
+        <li class="link" v-for="link in links" v-html="link.label" v-on:click="fetchProjectsByUrl(link.url)">
+        </li>
+    </ul>
 </template>
 
 <!-- CSS -->
@@ -56,5 +89,17 @@
         display: flex;
         flex-direction: column;
         gap: 20px;
+    }
+    /* Links */
+    .links {
+        margin-top: 20px;
+        display: flex;
+        gap: 15px;
+    }
+    /* Link */
+    .link {
+        box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        padding: 5px;
+        cursor: pointer;
     }
 </style>
